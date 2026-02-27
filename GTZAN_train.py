0e7674ad1cee203894a9612027494127303c08c4
mmvp314 <215004514+mmvp314@users.noreply.github.com>
2026-02-27 09:11:17 +0000

    Pass architecture to parse_args

--- GTZAN_train.py
+++ GTZAN_train.py
@@ -3,17 +3,25 @@ import torch.optim as optim
 import argparse
 
 from src.dataset import classes, get_datasets, get_dataloaders
-from src.model import audioCNN
+from src.model import audioCNN, audioCNN2
 from src.train import train_CNN, plot_history
+
+model_choices = {'audioCNN': audioCNN, 'audioCNN2': audioCNN2}
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Train a music genre classifier.")
-    parser.add_argument("--data-dir",   type=str,   default="./data")
-    parser.add_argument("--val-split",  type=float, default=0.1)
-    parser.add_argument("--test-split", type=float, default=0.1)
-    parser.add_argument("--batch-size", type=int,   default=64)
-    parser.add_argument("--lr",         type=float, default=1e-3)
-    parser.add_argument("--epochs",     type=int,   default=20)
+    parser.add_argument("--data-dir",     type=str,   default="./data")
+    parser.add_argument("--val-split",    type=float, default=0.1)
+    parser.add_argument("--test-split",   type=float, default=0.1)
+    parser.add_argument("--batch-size",   type=int,   default=64)
+    parser.add_argument("--lr",           type=float, default=1e-3)
+    parser.add_argument("--epochs",       type=int,   default=20)
+    parser.add_argument("--architecture", type=str,   default='audioCNN',
+                    choices=model_choices.keys(),
+                    help="Model architecture to train. Choices:" \
+                    "audioCNN (3 convo layers, 3 dense layers)," \
+                    "audioCNN2 (2 convo layers, 3 dense layers - slower to train," \
+                    "audioCNN3 (4 convo layers, 4 dense layers))")
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -21,7 +29,7 @@ if __name__ == "__main__":
 
     args = parse_args()
 
-    model = audioCNN()
+    model = model_choices[args.architecture]()
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
     
